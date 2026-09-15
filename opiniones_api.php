a39633dfa38c69e2fb8c44100ccc43516409a11e
Isac181004 <isacvasquez017@gmail.com>
2026-09-15 17:22:58 -0500

    Actualizar mensajes del módulo de opiniones JSON

--- opiniones_api.php
+++ opiniones_api.php
@@ -24,7 +24,7 @@ function mc_opinion_public_csrf() {
 try {
     mc_opinions_install();
 } catch (Throwable $e) {
-    mc_opinion_json(false, 'El módulo de opiniones aún no puede conectarse a MySQL.', [
+    mc_opinion_json(false, 'El módulo de opiniones no puede abrir su archivo JSON de almacenamiento.', [
         'setup_required' => true,
     ], 503);
 }
